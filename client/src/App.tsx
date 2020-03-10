c357c2d3c6b11f558ffd4c8b32369bdb25bd8d48
Craig Robinson <raggy1995@gmail.com>
2020-03-10 22:54:40 +1100

    created console component for the 6 flight directors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStyle } from './components/GlobalStyle';
 import { StarBackground } from './components/StarBackground'
 import Menu from './components/menu/Menu'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Tabs from 'react-bootstrap/Tabs'
-import Tab from 'react-bootstrap/Tab'
-import spartan_1 from "./media/Spartan_1.png"
-import spartan_2 from "./media/Spartan_2.png"
-import {Spartan} from './components/console/spartan';
-import {Capcom} from './components/console/capcom';
-import {Cronos} from './components/console/cronos';
-import {Ethos} from './components/console/ethos';
-import {Flight} from './components/console/flight';
-import {Bme} from './components/console/bme';
+import { Console } from './components/console/console'
+
 
 interface AppProps {};
 
@@ -21,32 +12,7 @@ const App: React.FC<AppProps> = () => {
   return(
     <>
       <GlobalStyle />
-      {/* <Menu /> */}
-
-      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example"  className="flex-column">
-        
-        <Tab eventKey="SPARTAN" title="SPARTAN">
-          <Spartan />
-        </Tab>
-        <Tab eventKey="CRONOS" title="CRONOS">
-          <Cronos />
-        </Tab>
-        <Tab eventKey="ETHOS" title="ETHOS">
-          <Ethos />
-        </Tab>
-        <Tab eventKey="FLIGHT" title="FLIGHT">
-         <Flight />
-        </Tab>
-        <Tab eventKey="CAPCOM" title="CAPCOM">
-          <Capcom />
-        </Tab>
-        <Tab eventKey="BME" title="BME">
-          <Bme />
-        </Tab>
-       
-      </Tabs>
-
-
+      <Console />
       <StarBackground />
     </>
   )
---
+++ client/src/components/console/console.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+import {Spartan} from './spartan';
+import {Capcom} from './capcom';
+import {Cronos} from './cronos';
+import {Ethos} from './ethos';
+import {Flight} from './flight';
+import {Bme} from './bme';
+
+interface AppProps {};
+
+const console: React.FC<AppProps> = () => {
+  return(
+    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example"  className="flex-column">
+        
+      <Tab eventKey="SPARTAN" title="SPARTAN">
+        <Spartan />
+      </Tab>
+      <Tab eventKey="CRONOS" title="CRONOS">
+        <Cronos />
+      </Tab>
+      <Tab eventKey="ETHOS" title="ETHOS">
+        <Ethos />
+      </Tab>
+      <Tab eventKey="FLIGHT" title="FLIGHT">
+      <Flight />
+      </Tab>
+      <Tab eventKey="CAPCOM" title="CAPCOM">
+        <Capcom />
+      </Tab>
+      <Tab eventKey="BME" title="BME">
+        <Bme />
+      </Tab>
+   
+  </Tabs>
+  );
+}
+
+export const Console = console;
